863a8fa1761951e2fc292f858638c9ff5332b95f
Kropie <Kropie21@gmail.com>
2017-03-14 22:53:37 -0500

    Formatting Telivision class

--- IntmJ/workspace/Lab01.2/src/com/entertainment/Television.java
+++ IntmJ/workspace/Lab01.2/src/com/entertainment/Television.java
@@ -10,70 +10,74 @@ package com.entertainment;
 
 import java.util.Objects;
 
-public class Television implements Comparable<Television>{
-  private String brand;
-  private int volume;
-  private Tuner tuner = new Tuner();  // set up internally and used for channel management
-  
-  public Television() {
-  }
-  
-  public Television(String brand, int volume) {
-    setBrand(brand);
-    setVolume(volume);
-  }
-  
-  public String getBrand() {
-    return this.brand;
-  }
-  public void setBrand(String brand) {
-    this.brand = brand;
-  }
-  
-  public int getVolume() {
-    return this.volume;
-  }
-  public void setVolume(int volume) {
-    this.volume = volume;
-  }
-  
-  public int getCurrentChannel() {
-    return tuner.getChannel();  // delegate to contained Tuner object
-  }
-  
-  public void changeChannel(int channel) {
-    tuner.setChannel(channel);  // delegate to contained Tuner object
-  }
-  
-  public String toString() {
-    return getClass().getSimpleName() + " [brand=" + getBrand() + ", volume=" + getVolume() + ", currentChannel=" + getCurrentChannel() + "]";
-  }
+public class Television implements Comparable<Television> {
+	private String brand;
+	private int volume;
+	private Tuner tuner = new Tuner(); // set up internally and used for channel
+										// management
 
-/**
- * Objects.hash was introduced in Java 7.
- * 
- * @see java.lang.Object#hashCode()
- */
-@Override
-public int hashCode() {
-	return Objects.hash(getBrand(), getVolume(), getCurrentChannel());
-}
-
-@Override
-public boolean equals(Object obj) {
-	boolean result = false;
-	if(obj instanceof Television) {
-		Television other = (Television) obj;
-		result = Objects.equals(this.getBrand(), other.getBrand())
-				&& Objects.equals(this.getCurrentChannel(), other.getCurrentChannel())
-				&& Objects.equals(this.getVolume(), other.getVolume());
+	public Television() {
 	}
-	return result;
-}
+
+	public Television(String brand, int volume) {
+		setBrand(brand);
+		setVolume(volume);
+	}
+
+	public String getBrand() {
+		return this.brand;
+	}
+
+	public void setBrand(String brand) {
+		this.brand = brand;
+	}
+
+	public int getVolume() {
+		return this.volume;
+	}
+
+	public void setVolume(int volume) {
+		this.volume = volume;
+	}
+
+	public int getCurrentChannel() {
+		return tuner.getChannel(); // delegate to contained Tuner object
+	}
+
+	public void changeChannel(int channel) {
+		tuner.setChannel(channel); // delegate to contained Tuner object
+	}
+
+	public String toString() {
+		return getClass().getSimpleName() + " [brand=" + getBrand() + ", volume=" + getVolume() + ", currentChannel="
+				+ getCurrentChannel() + "]";
+	}
 
 	/**
-	 * This will first sort by the brand. If the brands are equivalent then this will sort by
-	 * the channel.
+	 * Objects.hash was introduced in Java 7.
+	 * 
+	 * @see java.lang.Object#hashCode()
+	 */
+	@Override
+	public int hashCode() {
+		return Objects.hash(getBrand(), getVolume(), getCurrentChannel());
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		boolean result = false;
+		if (obj instanceof Television) {
+			Television other = (Television) obj;
+			result = Objects.equals(this.getBrand(), other.getBrand())
+					&& Objects.equals(this.getCurrentChannel(), other.getCurrentChannel())
+					&& Objects.equals(this.getVolume(), other.getVolume());
+		}
+		return result;
+	}
+
+	/**
+	 * This will first sort by the brand. If the brands are equivalent then this
+	 * will sort by the channel.
 	 * 
 	 * 
 	 * @see java.lang.Comparable#compareTo(java.lang.Object)
